aa9fb55390d9121ec753c9d6c711ed1373f10fc3
Agrima <qvistn111@gmail.com>
2022-01-02 21:16:16 +0530

    .

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import { Square } from "../Square/Square";
 export function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setxIsNext] = useState(true);
+
+  console.log(squares);
+
+  const handleClick = (item: any, index: number) => {
+    console.log("clicked", item, index);
+    const temp = squares.slice();
+    temp[index] = xIsNext ? "X" : "0";
+    setSquares(temp);
+    setxIsNext(!xIsNext);
+  };
+
+  const renderSquare = (item: any, index: number) => {
+    const onPressSquare = () => {
+      handleClick(item, index);
+    };
+    return <Square value={item} onPress={onPressSquare} />;
+  };
+
+  const status = "Next Player :" + (xIsNext ? " X " : " 0 ");
+
   return (
-    <div className="w-96 h-96 border grid grid-cols-3 grid-rows-3">
-      <Square value="X" />
-      <Square value="0" />
-      <Square value="X" />
-      <Square value="0" />
-      <Square value="X" />
-      <Square value="0" />
-      <Square value="X" />
-      <Square value="0" />
-      <Square value="X" />
+    <div>
+      <div>{status}</div>
+      <div className="w-96 h-96 border grid grid-cols-3 grid-rows-3">
+        {squares.map(renderSquare)}
+      </div>
     </div>
   );
 }
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,7 +1,18 @@
-type PropTypes = {
-    value: string;
-}
+import { useState } from "react";
 
-export function Square(props: PropTypes){
-    return ( <button className="border w-32 h-32">{props.value}</button>)
+type PropTypes = {
+  value: string;
+  onPress: any;
+};
+
+export function Square(props: PropTypes) {
+  const onClick = () => {
+    props.onPress();
+  };
+
+  return (
+    <button className="border w-32 h-32" onClick={onClick}>
+      {props.value}
+    </button>
+  );
 }
