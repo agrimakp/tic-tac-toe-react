fd82e579f86dc2dab95462636c75e88b532c0b21
Agrima <qvistn111@gmail.com>
2022-01-03 11:12:29 +0530

    added reset functionality

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import { calculateWinner } from "../../ticTacToe";
 import { Square } from "../Square/Square";
 export function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setxIsNext] = useState(true);
 
-  console.log(squares);
 
   const handleClick = (item: any, index: number) => {
-    console.log("clicked", item, index);
     const temp = squares.slice();
+    //ignore a click if someone wins
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
+
     temp[index] = xIsNext ? "X" : "0";
     setSquares(temp);
     setxIsNext(!xIsNext);
@@ -21,14 +25,30 @@ export function Board() {
     return <Square value={item} onPress={onPressSquare} />;
   };
 
-  const status = "Next Player :" + (xIsNext ? " X " : " 0 ");
+
+  const reset = () => {
+    setSquares(Array(9).fill(null));
+    setxIsNext(true);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
 
   return (
     <div>
       <div>{status}</div>
-      <div className="w-96 h-96 border grid grid-cols-3 grid-rows-3">
+      <div className="w-96 h-96 border grid grid-cols-3 grid-rows-3 mt-4">
         {squares.map(renderSquare)}
       </div>
+      <button className="border text-red-600 mt-5 p-2" onClick={reset}>RESET</button>
     </div>
   );
 }
+
+
