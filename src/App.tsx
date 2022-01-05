314b393b0a665ae516e28485416d6edfceeecde8
Agrima <qvistn111@gmail.com>
2022-01-05 10:57:12 +0530

    removed logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { useState } from 'react'
-import logo from './tic-tac-toe.png'
-import './App.css'
-import {Board} from './components/Board/Board'
+import "./App.css";
+import { Board } from "./components/Board/Board";
 
 function App() {
- 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>TIC TAC TOE</p>
-    
-        <Board/>
+
+        <Board />
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
